4b1ca3cdcf9c38d41434ff8c34a2f4bf9902c2fe
andre <andresalexanderhq@gmail.com>
2024-10-13 15:58:15 -0500

    primer fase de auth

--- src/main/java/com/apitv/proyecto/controller/TestController.java
+++ src/main/java/com/apitv/proyecto/controller/TestController.java
@@ -1,4 +1,20 @@
 package com.apitv.proyecto.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class TestController {
+
+    @GetMapping("/testfree")
+    public String freeAcess() {
+        return "Acceso libre, sin autentificacion necesaria";
+    }
+
+    @GetMapping("/testsecure")
+    public String secureAccess() {
+        return "Acceso privado, autenticacion requerida";
+    }
 }
